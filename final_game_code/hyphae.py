e7f01473e6e4642807810ea8fd9eb7effd0015c1
chdorgeix1 <112966997+chdorgeix1@users.noreply.github.com>
2023-04-05 16:48:26 -0400

    Finalized hyphae.py

--- final_game_code/hyphae.py
+++ final_game_code/hyphae.py
@@ -1,9 +1,9 @@
 # This file will contain the hyphae classes for the game
 import pygame
-import random
 from abc import ABC, abstractmethod
+from pygame.sprite import Sprite
 
-class BaseHyphae(ABC, pygame.sprite.Sprite):
+class BaseHyphae(ABC, Sprite):
     def __init__(self, color, height, width):
         super().__init__()
         self.color = color
@@ -17,16 +17,12 @@ class BaseHyphae(ABC, pygame.sprite.Sprite):
     def move(self):
         pass
 
-    @abstractmethod
-    def paint_kind(self):
-        pass
-
 class DurableHyphae(BaseHyphae):
     """
     A sprite representing a durable hyphae.
     """
 
-    def __init__(self, kind = 'durable_fungi', paintkind = 'chitin_fungi'):
+    def __init__(self):
         """
         Initialize a new instance of the DurableHyphae class.
 
@@ -37,21 +33,10 @@ class DurableHyphae(BaseHyphae):
         kind (str): the kind of the hyphae.
         paintkind (str): the paint kind of the hyphae.
         """
-        super().__init__(color = (255,0,0), height = 20, width = 20)
-        self.kind = kind
-        self.paintkind = paintkind
+        super().__init__(color = (0,255,0), height = 20, width = 20)
         self.image = pygame.Surface([self.width, self.height])
         self.image.fill(self.color)
         self.rect = self.image.get_rect()
-
-    def paint_kind(self, world_sprite_dict, player_trait_point_dict):
-        """
-        Paint the hyphae with the current paint kind.
-        """
-        sprite = sprite_dict[self.rect.x, self.rect.y]
-        if sprite.kind == 'food_sprite':
-            trait_point_dict[self.paintkind[-1:]] += 1
-        sprite.setKind(self.paintkind)
 
     def move(self, dx, dy):
         """
@@ -61,17 +46,47 @@ class DurableHyphae(BaseHyphae):
         dx (int): the number of pixels to move the hyphae in the x direction.
         dy (int): the number of pixels to move the hyphae in the y direction.
         """
-        dest_x = self.rect.x + dx
-        dest_y = self.rect.y + dy
         self.rect.move_ip(dx, dy)
-        self.paint_kind()
+        return(self.rect.x, self.rect.y)
 
 class PoisonHyphae(BaseHyphae):
     """
     A sprite representing a durable hyphae.
     """
 
-    def __init__(self, kind = 'durable_fungi', paintkind = 'chitin_fungi'):
+    def __init__(self):
+        """
+        Initialize a new instance of the DurableHyphae class.
+
+        Parameters:
+        color (tuple): the RGB color of the hyphae.
+        height (int): the height of the hyphae in pixels.
+        width (int): the width of the hyphae in pixels.
+        kind (str): the kind of the hyphae.
+        paintkind (str): the paint kind of the hyphae.
+        """
+        super().__init__(color = (0,0,255), height = 20, width = 20)
+
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+
+    def move(self, dx, dy):
+        """
+        Move the hyphae by the given amount of pixels in the x and y directions.
+
+        Parameters:
+        dx (int): the number of pixels to move the hyphae in the x direction.
+        dy (int): the number of pixels to move the hyphae in the y direction.
+        """
+        self.rect.move_ip(dx, dy)
+        return(self.rect.x, self.rect.y)
+    
+class SlimeHyphae(BaseHyphae):
+    """
+    A sprite representing a durable hyphae.
+    """
+    def __init__(self):
         """
         Initialize a new instance of the DurableHyphae class.
 
@@ -83,22 +98,9 @@ class PoisonHyphae(BaseHyphae):
         paintkind (str): the paint kind of the hyphae.
         """
         super().__init__(color = (255,0,0), height = 20, width = 20)
-        self.kind = kind
-        self.paintkind = paintkind
         self.image = pygame.Surface([self.width, self.height])
         self.image.fill(self.color)
         self.rect = self.image.get_rect()
-
-    def paint_kind(self, world_sprite_dict, player_trait_point_dict):
-        if world_sprite_dict[self.rect.x, self.rect.y].kind == 'food_sprite':
-            player_trait_point_dict[self.paintkind[0][-1:]] += 1
-        if world_sprite_dict[self.rect.x, self.rect.y].kind == 'exp_sprite_1' or world_sprite_dict[self.rect.x, self.rect.y].kind == 'exp_sprite_2':
-            None
-        else:
-            if random.random() < 0.75:
-                world_sprite_dict[self.rect.x, self.rect.y].setKind(self.paintkind[0])
-            else:
-                world_sprite_dict[self.rect.x, self.rect.y].setKind(self.paintkind[1])
 
     def move(self, dx, dy):
         """
@@ -108,7 +110,5 @@ class PoisonHyphae(BaseHyphae):
         dx (int): the number of pixels to move the hyphae in the x direction.
         dy (int): the number of pixels to move the hyphae in the y direction.
         """
-        dest_x = self.rect.x + dx
-        dest_y = self.rect.y + dy
         self.rect.move_ip(dx, dy)
-        self.paint_kind()
+        return(self.rect.x, self.rect.y)
